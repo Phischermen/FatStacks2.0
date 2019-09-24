145295827c6384f151e8afc6ba951807609c32c8
Phischermen <kevfischermen@gmail.com>
2019-09-23 18:47:16 -0700

    Imported my first Asset.

--- FatStacks/Assets/LevelDecorations/Scripts/Gear.cs
+++ FatStacks/Assets/LevelDecorations/Scripts/Gear.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Gear : MonoBehaviour
 {
+    public GameObject gear;
     public float initSpeed;
-    public float Speed { get { return GetComponent<Animator>().speed; } set { GetComponent<Animator>().speed = value; } }
+    public float Speed { get { return gear.GetComponent<Animator>().speed; } set { gear.GetComponent<Animator>().speed = value; } }
 
     private void Start()
     {
